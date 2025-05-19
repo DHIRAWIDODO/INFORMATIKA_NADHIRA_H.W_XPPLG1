3f2c907d735d22bcfc63a44f1b0924557a826895
4raaaapart <dhirayyuwidodo@gmail.com>
2025-05-19 09:44:51 +0700

    Tugas Kalkulator
    tugas kalkulator sederhana

---
+++ kkl.py
@@ -0,0 +1,15 @@
+adira = 10
+bdira = 20
+opdira = '/'
+
+if (opdira == '+'):
+    hadira = (adira + bdira)
+elif (opdira == '-'):
+    hadira = (adira - bdira)
+elif (opdira == '*'):
+    hadira = (adira * bdira)
+elif (opdira == '/'):
+    hadira = (adira / bdira)
+else:
+    print("Inputan salah")
+print("Hasil =", hadira)
